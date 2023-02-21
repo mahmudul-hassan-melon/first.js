87a2d8e09c92391d227741dcf744ecaca8e68a0b
mahmudul-hassan-melon <mahmudulhassanmelon@gmail.com>
2023-02-21 20:50:59 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,46 @@
+// 1.variable name can not be any keywords. 
+// 2.variable name has to be in one word.No space 
+// 3.variable name can not have quotation 
+// 4.variable name can not start with a number but can ends with a number 
+
+// Numaric
+let price = 13;
+console.log(price);
+
+// String 
+
+let person ="Mahmud Hassan";
+console.log(person);
+
+// Boolean
+
+let serious = true;
+console.log(serious);
+
+// Operation
+
+let onionPrice = 50;
+let eggPrice = 30;
+
+// addition
+let totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+
+// subtraction
+
+let priceDifferance = onionPrice - eggPrice;
+console.log(priceDifferance);
+
+// Multiplication
+
+let totalCost = onionPrice * eggPrice;
+console.log(totalCost);
+
+// division
+
+let money  = 5000;
+let player = 10;
+let eachPlayer = money / player
+console.log(eachPlayer);
+
+
